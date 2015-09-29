ca339d39d6c3f35a870a0b9aa2e283f42a85a63e
Sheldon Martin <sheld0ri@gmail.com>
2015-09-29 18:11:31 -0400

    Makes correction to Board#possible_moves spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,7 +4,7 @@ describe Board do
   describe "#possible_moves" do
     it "should return a list of possible moves" do
       board = Board.new
-      expect(board.class).to eq(Board)
+      expect(board.possible_moves).to eq(['a3','b3', 'c3', 'a2','b2', 'c2', 'a1','b1', 'c1'])
     end
   end
 end

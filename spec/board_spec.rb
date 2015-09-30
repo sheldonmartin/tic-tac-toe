5dd7dfca3d2e9ff06c73cdf38a338ddfb6b121f3
Sheldon Martin <sheld0ri@gmail.com>
2015-09-30 12:51:46 -0400

    Refactors board_spec.rb

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,16 +1,16 @@
 require_relative '../src/board'
 
 describe Board do
+  let(:board) { Board.new }
+
   describe "#to_s" do
     it "should return a board as a string" do
-      board = Board.new
       expect(board.to_s).to eq("3| | | |\n2| | | |\n1| | | |\n  a b c")
     end
   end
 
   describe "#mark_location" do
     it "should mark the board at the correct location" do
-      board = Board.new
       board.mark_location('b3', 'X')
 
       expect(board.to_s).to eq("3| |X| |\n2| | | |\n1| | | |\n  a b c")
@@ -19,7 +19,6 @@ describe Board do
 
   describe "#possible_moves" do
     it "should return a list of possible moves" do
-      board = Board.new
       expect(board.possible_moves).to eq(['a3','b3', 'c3', 'a2','b2', 'c2', 'a1','b1', 'c1'])
     end
   end

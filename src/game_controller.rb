afc48220e8522d9f4a357f99ec5498da4a5e7fde
Sheldon Martin <sheld0ri@gmail.com>
2015-09-30 21:29:23 -0400

    Players can now select their symbol

--- src/game_controller.rb
+++ src/game_controller.rb
@@ -1,16 +1,23 @@
 require_relative 'view'
 
-class GameController
-  
+class GameController  
   def initialize(board)
     @board = board
+    @players = ['X', 'O']
   end
 
   def start
     View.introduction_message
+    symbol = ""
+
+    until players.include?(symbol)
+      symbol = View.select_symbol.chomp.upcase
+    end
+
+    View.symbol_selection(symbol)
   end
 
   private
-  
-  attr_reader :board
+
+  attr_reader :board, :players
 end
--- src/view.rb
+++ src/view.rb
@@ -2,4 +2,13 @@ module View
   def self.introduction_message
     puts "Welcome to Tic-Tac-Toe"
   end
+
+  def self.select_symbol
+    puts "Please select either 'X' or 'O' to select your symbol and start the game? (type 'X' or 'O')"
+    return gets
+  end
+
+  def self.symbol_selection(symbol)
+    puts "You have selected to be #{symbol}"
+  end
 end
